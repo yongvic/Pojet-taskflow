1adec54214f9ab480d181d695e60531e48344a38
CHOCO <edo.sokpa@lomebs.com>
2025-03-27 09:42:33 +0100

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes";
-import "./styles/styles.css"; // Si tu as un fichier CSS
+import "./styles/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
