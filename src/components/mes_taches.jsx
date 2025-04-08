c513e6e2d102e8c9c26a348a9a4ce9b2b1b3f364
CHOCO <edo.sokpa@lomebs.com>
2025-04-08 05:40:01 +0200

    Avancé 2

--- src/components/mes_taches.jsx
+++ src/components/mes_taches.jsx
@@ -4,6 +4,10 @@ import "../styles/mes_taches.css"
 import board from "../assets/board.svg"
 import calendar from "../assets/calendar.svg"
 import plus from "../assets/plus.svg"
+import point from "../assets/point.svg"
+import terminer from "../assets/terminer.png"
+import en_cours from "../assets/en_cours.png"
+import not_start from "../assets/not_start.png"
 
 export default function MesTaches() {
     return (
@@ -50,6 +54,81 @@ export default function MesTaches() {
                             <img src={plus} alt="Ajouter" className="img_new" />
                         </div>
                     </div>
+
+                    <div className="process">
+                        <div>
+                            <div className="process1">
+                                <img src={point} alt="point" className="point1" />
+                                <p className="p">Non commencé</p>
+                            </div>
+
+                            <div className="pam">
+
+                                <div className="pam1">
+                                    <img src={not_start} alt="Not start" className="img_1" />
+                                    <p>J'ai un projet</p>
+                                </div>
+                                <p className="p1">23 mars 2006</p>
+
+                            </div>
+
+                            <div className="pamar1">
+
+                                <div className="pam4">
+                                    <img src={plus} alt="Ajouter" className="imomo" />
+                                    <p>Créer un(e) item</p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div>
+                            <div className="process2">
+                                <img src={point} alt="point" className="point2" />
+                                <p className="pp">En cours</p>
+                            </div>
+
+                            <div className="pama">
+
+                                <div className="pam2">
+                                    <img src={en_cours} alt="Not start" className="img_2" />
+                                    <p >J'ai un projet</p>
+                                </div>
+                                <p className="p2">23 mars 2006</p>
+
+                            </div>
+
+                            <div className="pamar1">
+
+                                <div className="pam4">
+                                    <img src={plus} alt="Ajouter" className="imomo" />
+                                    <p>Créer un(e) item</p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div>
+                            <div className="process3">
+                                <img src={point} alt="point" className="point3" />
+                                <p className="ppp">Terminer</p>
+                            </div>
+
+                            <div className="pamar">
+
+                                <div className="pam3">
+                                    <img src={terminer} alt="Not start" className="img_3" />
+                                    <p>J'ai un projet</p>
+                                </div>
+                                <p className="p3">23 mars 2006</p>
+
+                            </div>
+                        </div>
+
+                    </div>
+
                 </div>
 
 
